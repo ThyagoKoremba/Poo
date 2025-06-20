92aacb361b762db9d92ae9973375a23f3f01260c
Thyago Koremba <tkoremba@gmail.com>
2025-06-20 12:30:37 -0300

    Base de base

---
+++ target/generated-sources/annotations/com/mycompany/tienda/LOGICA/Cajero_.java
@@ -0,0 +1,15 @@
+package com.mycompany.tienda.LOGICA;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-06-20T11:59:36", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Cajero.class)
+public class Cajero_ { 
+
+    public static volatile SingularAttribute<Cajero, Integer> id;
+    public static volatile SingularAttribute<Cajero, String> user;
+    public static volatile SingularAttribute<Cajero, String> contraseña;
+
+}
